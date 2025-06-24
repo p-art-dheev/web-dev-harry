445777b2e75f5ba1f7b193c62b21f379bf1f396e
p-art-dheev <pardheev.vatturu1234@gmail.com>
2025-06-24 17:10:58 +0530

    variables, objects, datatypes

---
+++ 55/script.js
@@ -0,0 +1,39 @@
+console.log("hello this is pardheev")
+
+var a =5;
+a = a +1;
+var b =6;
+var c = "Harry";
+var _a = "Shubham";
+// var 55a = "Rohan"; 19 NOT ALLOWED
+
+console.log(a + b + 8);
+console.log(typeof a, typeof b, typeof c);
+
+// const a1 = 6;
+// a1 = a1 +5; NOT ALLOWED, we cannot assing values to a constant 
+
+// always you let in modern js
+// var is global
+// let and const are block
+
+const q = false;
+let r = 22;
+let s = 2.55;
+let t = true;
+let u = undefined;
+let v = null;
+
+console.log(q, r, s, t, u, v)
+console.log(typeof q, typeof r, typeof s, typeof t, typeof u, typeof v)
+
+
+let list = {
+    name: Pardheev, "job-code": 5600, "is unemployed": true
+
+}
+
+console.log(list);
+list.salary = "100crores"
+console.log(list);
+list.salary ="500crores"
