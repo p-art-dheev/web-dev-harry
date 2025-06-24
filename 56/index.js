f9858c3abe62a72d28b4916188305fb086725d00
p-art-dheev <pardheev.vatturu1234@gmail.com>
2025-06-24 18:08:41 +0530

    if, else if

---
+++ 56/index.js
@@ -0,0 +1,24 @@
+console.log("I am conditional tutorial")
+
+let age = 4;
+let grace = 2;
+
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)
+
+if ((age**grace)>18){
+    console.log("You can drive")
+}
+
+else{
+    console.log("You cannot drive!!")
+}
+
+
+let num1 =10;;
+let num2 =9;
+let check = (num1<num2) ? num1+num2: num2-num1
+console.log("your number is " + check)
