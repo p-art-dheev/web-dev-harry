df51f1b9e113fe66b1faf2bf5d004178a2d87039
p-art-dheev <pardheev.vatturu1234@gmail.com>
2025-06-24 16:33:39 +0530

    js installation

---
+++ 54/script.js
@@ -0,0 +1,23 @@
+alert("hello there")
+
+console.log("This is my first video on javascript!!")
+console.log("This is my first video on javascript!!")
+console.log("This is my first video on javascript!!")
+console.log("This is my first video on javascript!!")
+
+
+var a = prompt("Enter your number")
+
+var istrue = confirm("Are you sure about leaving this page and blast your computer")
+
+if(istrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("you are safe")
+}
+
+console.log("Your number is "+ a)
+document.title="Pardheev"
+document.body.style.backgroundColor = "yellow"
