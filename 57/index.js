7e0fb896d824ebab495f9bbd066fd63534c063e1
p-art-dheev <pardheev.vatturu1234@gmail.com>
2025-07-02 15:57:25 +0530

    loops

---
+++ 57/index.js
@@ -0,0 +1,42 @@
+console.log("I am a tutorial on loops")
+
+let a = 1 
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+// console.log(a+3)
+
+// for (let i = 0; i < 1000; i++){
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     name: "Pardheev", role: "Programmer", company: "CodeWithHarry AI"
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(element)
+// }
+
+
+
+// for (const c of "PARDHEEV"){
+
+//     console.log(c)
+// }
+
+// let b = 11;
+// while (b<20){
+//     console.log("hello hello hello hello")
+//     b++
+// }
+
+
+let c = 100;
+do {
+console.log("HELLO")   ; 
+    c++;
+}
+while(c < 110);
+    
