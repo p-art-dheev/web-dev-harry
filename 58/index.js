f28fcb8b70bda15fcf121f1778de9681f4829b34
p-art-dheev <pardheev.vatturu1234@gmail.com>
2025-07-04 18:15:32 +0530

    functions

---
+++ 58/index.js
@@ -0,0 +1,25 @@
+function nice(p_name) {
+    console.log("Hey " + p_name + ", How are you?")
+    console.log("Hey " + p_name + ", Nice to meet you")
+    console.log("Hey " + p_name + ", Good Work!")
+    console.log("Hey " + p_name + ", ")
+    console.log("Hey " + p_name + ", How are you?")
+    
+}
+
+let p_name = "Pardheev"
+nice(p_name);
+
+
+function sum(num1, num2) {
+    console.log("The sum of two numbers is " + (num1 + num2))
+    
+}
+
+sum(1, 11)
+
+const func1 = (x)=>{
+    console.log("I am an arrow function " + x)
+}
+
+func1(100)
